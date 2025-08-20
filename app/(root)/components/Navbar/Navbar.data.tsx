ca1400004768beb1dc783c9fc4ee163770b924aa
arco14 <arcodev07@gmail.com>
2025-08-20 12:13:22 -0600

    feat: navbar

---
+++ app/(root)/components/Navbar/Navbar.data.tsx
@@ -0,0 +1,14 @@
+
+export const links = [{
+    name: "How it works",
+    href: "#how-it-works"
+}, {
+    name: "Pracing",
+    href: "#pracing"
+},{
+    name: "Contact",
+    href: "#contact"
+},{
+    name: "Dashboard",
+    href: "/dashboard"
+}]
